6729aa464e9f3e52fd0e374ab345c37ba0af0b54
Rick Moore <rickmooredev@gmail.com>
2022-05-29 12:41:54 -0400

    more tweaks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,5 +12,17 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootstrap"
+import * as bootstrap from 'bootstrap'
 import "../stylesheets/application"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
