ef2deb0a796cb53b5dae3d3d77d62902894614c0
Rick Moore <rickmooredev@gmail.com>
2022-08-09 21:22:02 -0400

    remove ship code from report

--- app/models/warning_calculator.rb
+++ app/models/warning_calculator.rb
@@ -17,7 +17,7 @@ class WarningCalculator
     report = DepartureWarning.unique_excursion_ids.to_a.each_with_object([]) do |id, report|
       dw = DepartureWarning.find_by(excursion_id: id)
       num = DepartureWarning.where(excursion_id: id).select('distinct excursion_date').count
-      report[idx] = {num => "#{id};#{dw.excursion_name};#{dw.port_name};#{dw.ship_code};#{num}"}
+      report[idx] = {num => "#{id};#{dw.excursion_name};#{dw.port_name};#{num}"}
       idx += 1
     end
     report.sort{ |a,b| b.keys.first <=> a.keys.first }.map{ |h| h.values.first }
